53aeffdf25934ad3ead6b25a0a8ae3fe7df9447e
Michael F. Schönitzer <michael@schoenitzer.de>
2019-09-01 22:21:30 +0200

    Initial version [WIP]

---
+++ app.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the Toolforge flask WSGI tutorial
+#
+# Copyright (C) 2017 Bryan Davis and contributors
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import urllib
+
+import flask
+import LexData
+import mwoauth
+import mysql.connector
+import requests
+import yaml
+from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from requests_oauthlib import OAuth1
+
+db_user = "michi"
+db_passwd = "mysqlpass"
+db_name = "machtsinn"
+db_table_main = "matches"
+
+app = flask.Flask(__name__)
+db = mysql.connector.connect(
+    host="localhost", user=db_user, passwd=db_passwd, database=db_name
+)
+
+wdqurl = "https://query.wikidata.org/sparql?format=json&query="
+wdapiurl = "https://www.wikidata.org/w/api.php"
+
+# Load configuration from YAML file
+__dir__ = os.path.dirname(__file__)
+app.config.update(yaml.safe_load(open(os.path.join(__dir__, "config.yaml"))))
+consumer_token = mwoauth.ConsumerToken(
+    app.config["CONSUMER_KEY"], app.config["CONSUMER_SECRET"]
+)
+
+
+def runquery(query):
+    url = wdqurl + urllib.parse.quote_plus(query)
+    r = requests.get(url)
+    if r.status_code == 200:
+        return r.json()["results"]
+    else:
+        return None
+
+
+def get_tokens(tokentype, auth):
+    params = {"action": "query", "format": "json", "meta": "tokens", "type": tokentype}
+    req = requests.get(wdapiurl, params=params, auth=auth)
+    if req.status_code != 200:
+        raise Exception("Unknown API-error")
+    result = req.json()
+    if "error" in result:
+        raise Exception("API-error", result["error"])
+    token = result["query"]["tokens"][tokentype + "token"]
+    return token
+
+
+@app.route("/")
+def index():
+    username = flask.session.get("username", None)
+    return flask.render_template("index.html", username=username, greeting="test")
+
+
+@app.route("/getcandidates")
+def getcandidates():
+    lang = request.args.get("lang", 188)
+    number = request.args.get("lang", 10)
+    cursor = db.cursor()
+    cursor.execute(
+        """SELECT lemma, matches.QID, LID, matches.lang, gloss FROM matches
+        JOIN labels
+            on matches.QID = labels.QID and matches.lang = labels.lang
+        WHERE
+            status = 0 and matches.lang = %s
+        LIMIT %s""",
+        (lang, number),
+    )
+    results = cursor.fetchall()
+    username = flask.session.get("username", None)
+    return flask.render_template("index.html", username=username, greeting=str(results))
+
+
+@app.route("/save", methods=["POST"])
+def save():
+    lid = request.args.get("LID")
+    qid = request.args.get("QID")
+    gloss = request.args.get("gloss")
+    access_token = flask.session["access_token"]
+    auth1 = OAuth1(
+        consumer_token.key,
+        client_secret=consumer_token.secret,
+        resource_owner_key=access_token["key"],
+        resource_owner_secret=access_token["secret"],
+    )
+    repo = LexData.WikidataSession(token=auth1)
+
+    L = LexData.Lexeme(repo, lid)
+    if L.senses:
+        pass  # FIXME
+    print('Lexeme {} "{}"'.format(lid, L.lemma))
+
+    glosses = {"de": gloss}
+    claims = {"P5137": [qid]}
+    try:
+        _ = L.createSense(glosses, claims)
+    except PermissionError:
+        return "Error!"  # FIXME
+    return "Done!"
+
+
+@app.route("/login")
+def login():
+    """Initiate an OAuth login.
+
+    Call the MediaWiki server to get request secrets and then redirect the
+    user to the MediaWiki server to sign the request.
+    """
+    try:
+        redirect, request_token = mwoauth.initiate(
+            app.config["OAUTH_MWURI"], consumer_token
+        )
+    except Exception:
+        app.logger.exception("mwoauth.initiate failed")
+        return flask.redirect(flask.url_for("index"))
+    else:
+        flask.session["request_token"] = dict(zip(request_token._fields, request_token))
+        return flask.redirect(redirect)
+
+
+@app.route("/oauth-callback")
+def oauth_callback():
+    """OAuth handshake callback."""
+    if "request_token" not in flask.session:
+        flask.flash(u"OAuth callback failed. Are cookies disabled?")
+        return flask.redirect(flask.url_for("index"))
+
+    try:
+        access_token = mwoauth.complete(
+            app.config["OAUTH_MWURI"],
+            consumer_token,
+            mwoauth.RequestToken(**flask.session["request_token"]),
+            flask.request.query_string,
+        )
+
+        identity = mwoauth.identify(
+            app.config["OAUTH_MWURI"], consumer_token, access_token
+        )
+    except Exception:
+        app.logger.exception("OAuth authentication failed")
+
+    else:
+        flask.session["access_token"] = dict(zip(access_token._fields, access_token))
+        flask.session["username"] = identity["username"]
+
+    return flask.redirect(flask.url_for("index"))
+
+
+@app.route("/logout")
+def logout():
+    """Log the user out by clearing their session."""
+    flask.session.clear()
+    return flask.redirect(flask.url_for("index"))
---
+++ backend.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python3
+import time
+from typing import List, Tuple
+
+import mysql.connector
+import requests
+
+db_user = "michi"
+db_passwd = "mysqlpass"
+db_name = "machtsinn"
+db_table_main = "matches"
+db_table_texts = "labels"
+
+# Run a query against a web-api
+def runquery(url, params={}, session=requests):
+    headers = {"User-Agent": user_agent}
+    r = session.get(url, headers=headers, params=params)
+    if r.status_code == 200:
+        return r.json()["results"]
+    return None
+
+
+# Run a Spaql-Query
+def runSPARQLquery(query):
+    return runquery(endpoint_url, params={"format": "json", "query": query})["bindings"]
+
+
+endpoint_url = "https://query.wikidata.org/sparql"
+
+user_agent = "makesense 0.0.1 by User:MichaelSchoenitzer"
+
+with open("query.sparql") as f:
+    sparql = f.read()
+
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user=db_user,
+        passwd=db_passwd,
+        database=db_name,
+        charset="utf8",
+        use_unicode=True,
+    )
+except:
+    mydb = mysql.connector.connect(host="localhost", user=db_user, passwd=db_passwd)
+
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE DATABASE {}".format(db_name))
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user=db_user,
+        passwd=db_passwd,
+        database=db_name,
+        charset="utf8",
+        use_unicode=True,
+    )
+
+
+mycursor = mydb.cursor()
+
+mycursor.execute(
+    """CREATE TABLE IF NOT EXISTS `{}` (
+     `lang` INT,
+     `QID` INT,
+     `LID` INT,
+     `Status` INT,
+     PRIMARY KEY (`lang`,`QID`,`LID`)
+);""".format(
+        db_table_main
+    )
+)
+
+mycursor.execute(
+    """CREATE TABLE IF NOT EXISTS `{}` (
+     `lang` INT,
+     `QID` INT,
+     `lemma` TEXT CHARACTER SET utf8 NOT NULL,
+     `gloss` TEXT CHARACTER SET utf8 NOT NULL,
+     PRIMARY KEY (`lang`,`QID`)
+);""".format(
+        db_table_texts
+    )
+)
+
+print("Running Query…")
+res = runSPARQLquery(sparql)
+
+print("Collection results…")
+sql = "INSERT IGNORE INTO {} (lang, QID, LID, Status) VALUES (%s, %s, %s, %s)".format(
+    db_table_main
+)
+values: List[Tuple[int, int, int, int]] = []
+text_sql = "INSERT IGNORE INTO {} (lang, QID, lemma, gloss) VALUES (%s, %s, %s, %s)".format(
+    db_table_texts
+)
+text_values: List[Tuple[int, int, str, str]] = []
+for row in res:
+    lang = int(row["lang"]["value"][32:])
+    lid = int(row["lexeme"]["value"][32:])
+    qid = int(row["item"]["value"][32:])
+
+    lemma = row["lemma"]["value"]
+    desc = row["desc"]["value"]
+
+    values.append((lang, lid, qid, 0))
+    text_values.append((lang, lid, lemma, desc))
+
+print("Adding {} rows to Database…".format(len(values) + len(text_values)))
+
+try:
+    mycursor.executemany(sql, values)
+    mycursor.executemany(text_sql, text_values)
+except:
+    print(mycursor.statement)
+
+mydb.commit()
