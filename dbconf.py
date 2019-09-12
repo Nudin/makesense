7954a4a881fc8de6f49568ea15d6ef9c99f41563
Your Name <you@example.com>
2019-09-12 13:11:51 +0000

    Read db credentials from file

--- dbconf.py
+++ dbconf.py
@@ -1,6 +1,9 @@
-db_host = "localhost"
-db_user = "michi"
-db_passwd = "mysqlpass"
-db_name = "machtsinn"
+with open('/data/project/machtsinn/replica.my.cnf') as f:
+    lines = f.readlines()
+    db_passwd = lines[2].split()[-1]
+    db_user = lines[1].split()[-1]
+
+db_host = "tools.db.svc.eqiad.wmflabs"
+db_name db_user + "__machtsinn"
 db_table_main = "matches"
 db_table_texts = "labels"
